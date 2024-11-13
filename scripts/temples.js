14fd4b443e42fc441138bed9aba6ae68aae9b43b
scotty369 <scotty369@byui.edu>
2024-11-13 15:29:15 -0500

    Add

---
+++ scripts/temples.js
@@ -0,0 +1,5 @@
+// Update the current year in the footer
+document.getElementById("year").textContent = new Date().getFullYear();
+
+// Update the last modified date in the footer
+document.getElementById("last-modified").textContent = document.lastModified;
