68c1e4536ee7777a481f92d1d1d125e84ba47648
scotty369 <scotty369@byui.edu>
2024-11-14 19:12:23 -0500

    Add

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,13 @@
 // Update the current year in the footer
-document.getElementById("year").textContent = new Date().getFullYear();
+document.getElementById("currentyear").textContent = new Date().getFullYear();
 
 // Update the last modified date in the footer
 document.getElementById("last-modified").textContent = document.lastModified;
+
+// Hamburger menu toggle functionality
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector("header nav ul");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("visible");
+});
