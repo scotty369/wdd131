03bf9c4ea41691db7db9d99466c3cfc6571312d2
scotty369 <scotty369@byui.edu>
2024-11-19 20:59:50 -0500

    Add

--- scripts/place.js
+++ scripts/place.js
@@ -6,14 +6,14 @@ const temperature = 18; // Example Celsius temperature
 const windSpeed = 10; // Example wind speed in km/h
 const conditions = "Sunny"; // Example weather conditions
 
-document.getElementById("temperature").textContent = temperature;
-document.getElementById("windSpeed").textContent = windSpeed;
-document.getElementById("conditions").textContent = conditions;
+document.getElementById("Temperature").textContent = temperature;
+document.getElementById("WindSpeed").textContent = windSpeed;
+document.getElementById("Conditions").textContent = conditions;
 
 if (temperature <= 10 && windSpeed > 4.8) {
     const windChill =
         13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
-    document.getElementById("windChill").textContent = windChill.toFixed(1);
+    document.getElementById("WindChill").textContent = windChill.toFixed(1);
 } else {
-    document.getElementById("windChill").textContent = "N/A";
+    document.getElementById("WindChill").textContent = "N/A";
 }
